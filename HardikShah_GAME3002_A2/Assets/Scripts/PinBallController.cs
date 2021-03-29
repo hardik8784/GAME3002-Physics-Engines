061e68ae79e5a9ad4563a63d5344987adf3b903c
Hardik <hardik8784@gmail.com>
2021-03-29 15:03:07 -0400

    Added the re-spawning Pinball when go out of board

---
+++ HardikShah_GAME3002_A2/Assets/Scripts/PinBallController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinBallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "OuterArea")
+        {
+            GetComponent<Transform>().position = new Vector3(11.34f, 0.5f, 10.09f);
+        }
+    }
+}
