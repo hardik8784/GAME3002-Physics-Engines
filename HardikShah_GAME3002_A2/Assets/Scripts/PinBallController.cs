387c1b5104003d90662964aac38818848b02aaae
Hardik <hardik8784@gmail.com>
2021-03-30 12:44:09 -0400

    Added the Score And Live
    Second step is to Create a UI Element so that player can see the updates instantly.

--- HardikShah_GAME3002_A2/Assets/Scripts/PinBallController.cs
+++ HardikShah_GAME3002_A2/Assets/Scripts/PinBallController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PinBallController : MonoBehaviour
 {
+    public float Lives = 5.0f;
+    public float Score = 0.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,27 @@ public class PinBallController : MonoBehaviour
         if (other.tag == "OuterArea")
         {
             GetComponent<Transform>().position = new Vector3(11.34f, 0.5f, 10.09f);
+            Lives = Lives - 1;
+            Debug.Log("Lives Left:");
+            Debug.Log(Lives);
+        }
+
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Active_Bumper")
+        {
+            Score = Score + 100;
+            Debug.Log("Collide with Active_Bumper");
+            Debug.Log(Score);
+        }
+        if (collision.gameObject.tag == "Passive_Bumper")
+        {
+            Score = Score + 50;
+            Debug.Log("Collide with Passive_Bumper");
+            Debug.Log(Score);
         }
     }
 }
